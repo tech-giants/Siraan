bcb6889b1f07dbe340ba5613ffe67b3c18bdd908
tech-giants <umair@divsync.com>
2021-08-06 16:50:50 +0500

    QR code reader done

--- src/screens/QrScanner.js
+++ src/screens/QrScanner.js
@@ -1,6 +1,7 @@
 // import { View } from 'react-native';
 import { QRScannerView } from 'react-native-qrcode-scanner-view';
 import {
+  StyleSheet,
   ScrollView,
   RefreshControl,
   StatusBar,
@@ -12,40 +13,68 @@ import { Navigation } from 'react-native-navigation';
 import { registerDrawerDeepLinks } from '../utils/deepLinks';
 import React, { Component } from 'react';
 import * as nav from '../services/navigation';
+import SaldiriHeader from '../components/SaldiriComponents/SaldiriHeaderBar';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
 export default class DefaultScreen extends Component {
+  renderTitleBar = () => {
+    return (
+      <>
+        <SaldiriHeader
+          startComponent={
+            <Pressable
+              onPress={() => Navigation.dismissModal('QR_SCREEN')}
+              style={{
+                height: '100%',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <MaterialIcons name="arrow-back" size={20} color="#16191a" />
+            </Pressable>
+          }
+          midHeaderTitle="Scan QR code"
+        />
 
-    renderTitleBar = () => <Text style={{color:'white',textAlign:'center',padding:16}}>Title</Text>
+        {/* <Text style={{ color: 'white', textAlign: 'center', padding: 16 }}>
+        Title
+      </Text> */}
+      </>
+    );
+  };
+  renderMenu = () => (
+    <Text style={{ color: 'white', textAlign: 'center', padding: 16 }}>
+      Menu
+    </Text>
+  );
 
-    renderMenu = () => <Text style={{color:'white',textAlign:'center',padding:16}}>Menu</Text>
+  barcodeReceived = (event) => {
+    //   nav.selectTab("home");
+    Navigation.dismissModal('QR_SCREEN');
+    var deep_link = registerDrawerDeepLinks(
+      {
+        link: event.data,
+        payload: {
+          type: 'qr',
+          title: 'result',
+        },
+      },
+      'HOME_SCREEN',
+    );
 
-    barcodeReceived = (event) => {
-        //   nav.selectTab("home");
-        Navigation.dismissModal("QR_SCREEN");
-         var deep_link =  registerDrawerDeepLinks(
-                  {
-                    link: event.data,
-                    payload: {
-                      "type":"qr",
-                      title: "result",
-                    },
-                  },
-                  "HOME_SCREEN",
-        );
-      
-        // console.log('Type: ' + event.type + '\nData: ' + event.data)
-    };
+    // console.log('Type: ' + event.type + '\nData: ' + event.data)
+  };
 
-    render() {
-        return (
-           <View style={{flex:1}}>
-            < QRScannerView
-                onScanResult={ this.barcodeReceived }
-                renderHeaderView={ this.renderTitleBar }
-                    renderFooterView={this.renderMenu}
-                    hintText={"Scan QR CODE"}
-                    scanBarAnimateReverse={true}
-                />
-           </View>
-        )
-    }
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <QRScannerView
+          onScanResult={this.barcodeReceived}
+          renderHeaderView={this.renderTitleBar}
+          // renderFooterView={this.renderMenu}
+          hintText={'Scan QR CODE'}
+          scanBarAnimateReverse={true}
+        />
+      </View>
+    );
+  }
 }
